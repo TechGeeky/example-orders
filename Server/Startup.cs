62a295380ab6315d9f3c9b9a0ecc95376cf5ab31
TechGeeky <comptechgeeky@gmail.com>
2021-01-21 20:48:47 -0800

    Update Startup.cs

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Orders.Schema;
 using Orders.Services;
 
@@ -31,6 +32,11 @@ namespace Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+            
             services.AddSingleton<IOrderService, OrderService>();
             services.AddSingleton<ICustomerService, CustomerService>();
             services.AddTransient<OrderType>();
